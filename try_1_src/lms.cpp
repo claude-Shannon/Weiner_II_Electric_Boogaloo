24a83416234803d11825b32c2d51997868df4f31
Gregory Mueller <gregory.s.mueller@gmail.com>
2018-04-20 13:27:36 -0400

    static stuff and formatting
    added statics and consts to more variables.  Cleaned up some redundant code.
    
    Since this is hardware, shouldn't all of these variables be static anyway?

--- try_1_src/lms.cpp
+++ try_1_src/lms.cpp
@@ -4,42 +4,29 @@
 #include "ap_shift_reg.h"
 #include "fir.h"
 
+#define MAX 32 // customize our filter size here
 
-#define MAX 16
-
-
-
-
-
-ap_fixed<16,2> lms_hw(/*ap_fixed<16,2> *e,*/ ap_fixed<16,2> noise_in, ap_fixed<16,2> signal_noise)
+ap_fixed<16,2> lms_hw(ap_fixed<16,2> noise_in, ap_fixed<16,2> signal_noise)
 {
-	static ap_shift_reg<ap_fixed<16,2>, 32> y; //noise input shift register guy
-	ap_fixed<16,2> output = 0;
-	ap_fixed<16,2> mu = .125;
-	ap_fixed<16,2> h[32] = {0};
+	// maybe make these all statics? Hardware doesn't destroy variables like software?
+	
+	static ap_shift_reg<ap_fixed<16,2>, MAX> y; //noise input shift register guy
+	static ap_fixed<16,2> h[MAX]; //filter coeff's initialized to zero - static variables initialize to zero, hurray
+	static const ap_fixed<16,2> mu = .125; // mu is always .125 and should only be defined once
+	// perhaps make mu an input to change filter behavior on the fly???
+	ap_fixed<16,2> output = 0; //output value
+	static int p; //defined as zero in the loops
+	static ap_fixed<32,2> xhat; // defined as 0.0 in the loop
+	
+	y.shift(signal_noise); //shift in the current input to the top of the shift register
 
+	lms_label0:for (xhat=0.0, p=0; p<MAX-1; p++)
+			xhat += y.read(p)*h[p];
 
+	output = (signal_noise - ap_fixed<16,2>(xhat)); //cast xhat from 32,2 to 16,2
 
-int p =0;
+	lms_label1:for (p=0; p<MAX-1; p++)
+			h[p] += 2 * mu * output * y.read(p);
 
-ap_fixed<32,2> xhat;
-
-y.shift(signal_noise); //shift in the current input to the top of the shift register
-
-
-
-lms_label0:for (xhat=0.0, p=0; p<31; p++)
-	  xhat += y.read(p)*h[p];
-
-
-
-output = (signal_noise - ap_fixed<16,2>(xhat));
-
-
-lms_label1:for (p=0; p<31; p++)
-	  h[p] += 2 * mu * output * y.read(p);
-
-return output;
-
-
+	return output;
 }
