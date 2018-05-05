21ee106ea4a7462cd1e0fb31abbf022a4ac2807e
Gregory Mueller <gregory.s.mueller@gmail.com>
2018-05-05 18:29:42 -0400

    Update lms.cpp
    output is now a pointer and return value is void

--- try_1_src/lms.cpp
+++ try_1_src/lms.cpp
@@ -2,11 +2,12 @@
 // test lol
 #include "ap_fixed.h"
 #include "ap_shift_reg.h"
-#include "fir.h"
+#include "lms.h"
+
 
 #define MAX 32 // customize our filter size here
 
-ap_fixed<16,2> lms_hw(ap_fixed<16,2> noise_in, ap_fixed<16,2> signal_noise)
+void lms_hw (ap_fixed<16,2> noise_in, ap_fixed<16,2> signal_noise, ap_fixed<16,2>  *output)
 {
 	// maybe make these all statics? Hardware doesn't destroy variables like software?
 	
@@ -14,19 +15,19 @@ ap_fixed<16,2> lms_hw(ap_fixed<16,2> noise_in, ap_fixed<16,2> signal_noise)
 	static ap_fixed<16,2> h[MAX]; //filter coeff's initialized to zero - static variables initialize to zero, hurray
 	static const ap_fixed<16,2> mu = .125; // mu is always .125 and should only be defined once
 	// perhaps make mu an input to change filter behavior on the fly???
-	ap_fixed<16,2> output = 0; //output value
-	static int p; //defined as zero in the loops
+	//ap_fixed<16,2> output = ap_fixed<16,2>(0); //output value
+	int p; //defined as zero in the loops
 	static ap_fixed<32,2> xhat; // defined as 0.0 in the loop
 	
-	y.shift(signal_noise); //shift in the current input to the top of the shift register
+	y.shift(noise_in); //shift in the current input to the top of the shift register
 
 	lms_label0:for (xhat=0.0, p=0; p<MAX-1; p++)
 			xhat += y.read(p)*h[p];
 
-	output = (signal_noise - ap_fixed<16,2>(xhat)); //cast xhat from 32,2 to 16,2
+	*output = (signal_noise - ap_fixed<16,2>(xhat)); //cast xhat from 32,2 to 16,2
 
 	lms_label1:for (p=0; p<MAX-1; p++)
-			h[p] += 2 * mu * output * y.read(p);
+			h[p] += 2 * mu * (*output) * y.read(p);
 
-	return output;
+
 }
