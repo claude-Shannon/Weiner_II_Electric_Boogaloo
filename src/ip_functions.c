0567c664ad9a4deb691145d3cceedba5f2036d4b
Gregory Mueller <gregory.s.mueller@gmail.com>
2018-05-07 20:35:24 -0400

    Update ip_functions.c
    updates

--- src/ip_functions.c
+++ src/ip_functions.c
@@ -24,7 +24,7 @@
 // * ---------------------------------------------------------------------------- */
 //void lms_filter()
 //{
-//	u32 nco_in, nco_out, in_left, in_right, out_left, out_right, step, \
+//	u32 nco_in, nco_out, in_left, in_right, out_left, out_right, step,
 //		prevL, prevR, prevTone, temp;
 //
 //	/* Read step size value from DIP switches */
@@ -132,7 +132,7 @@ void mom_beat(void) //just mom
 	static const u32 mom_size = 51510; //maximum address value
 	static u32 address = 0;
 
-	u32 mom_out, in_left, prev_left, out_left;
+	u32 mom_out, in_left, prev_left, temp;
 
 	while (!XUartPs_IsReceiveData(UART_BASEADDR)){
 	/* Sample L+R audio from the codec */
@@ -158,28 +158,29 @@ void mom_beat(void) //just mom
 			/* Receive sinusoidal sample from NCO core */
 			mom_out = XNco_mom_Get_ekg_sample_V(&mom);
 
+			temp = mom_out<<8;
+
 			if(address==mom_size){
 				address=0;
-			}
+				}
 			else{
 				address=address+1;
-			}
+				}
 
 	//		/* Add scaled noise component to the L+R audio samples */
 	//		out_left =  temp + in_left;
 	//		out_right = temp + in_right;
 
 			/* Output corrupted audio to the codec */
-			Xil_Out32(I2S_DATA_TX_L_REG, mom_out);
-			Xil_Out32(I2S_DATA_TX_R_REG, mom_out);
-
-			prev_left = in_left;
+			Xil_Out32(I2S_DATA_TX_L_REG, temp);
+			Xil_Out32(I2S_DATA_TX_R_REG, temp);
 
 			//XNco_Get_sine_sample_V(&Nco);
 
 			//temp = nco_out;
+				prev_left = in_left;
+				}
 
-		}
 
 		/* If the DIP switch values have changed, break from while
 		 * loop to allow the step size value to update.
@@ -209,7 +210,7 @@ void baby_beat(void) //just mom
 	static const u32 baby_size = 19410; //maximum address value
 	static u32 address = 0;
 
-	u32 baby_out, in_left, prev_left, out_left;
+	u32 baby_out, in_left, prev_left;
 
 	while (!XUartPs_IsReceiveData(UART_BASEADDR)){
 	/* Sample L audio from the codec */
@@ -233,7 +234,7 @@ void baby_beat(void) //just mom
 			XNco_baby_Set_address_V(&baby, address);
 
 			/* Receive sinusoidal sample from NCO core */
-			baby_out = XNco_baby_Get_ekg_sample_V(&baby);
+			baby_out = XNco_baby_Get_ekg_sample_V(&baby)<<8;
 
 			if(address==baby_size){
 				address=0;
@@ -286,7 +287,7 @@ void both_beat(void) //both heart beats
 	static const u32 baby_size = 19410;
 	static u32 baby_address = 0;
 	static u32 mom_address = 0;
-	u32 baby_out, mom_out, in_left, prev_left, out_left;
+	u32 baby_out, mom_out, in_left, prev_left, temp;
 
 	while (!XUartPs_IsReceiveData(UART_BASEADDR)){
 	/* Sample L+R audio from the codec */
@@ -310,7 +311,7 @@ void both_beat(void) //both heart beats
 			XNco_mom_Set_address_V(&mom, mom_address);
 
 			/* Receive sinusoidal sample from NCO core */
-			mom_out = XNco_mom_Get_ekg_sample_V(&mom);
+			mom_out = XNco_mom_Get_ekg_sample_V(&mom)<<8;
 
 			if(mom_address==mom_size){
 				mom_address=0;
@@ -322,8 +323,8 @@ void both_beat(void) //both heart beats
 			XNco_baby_Set_address_V(&baby, baby_address);
 
 				/* Receive sinusoidal sample from NCO core */
-				baby_out = XNco_baby_Get_ekg_sample_V(&baby);
-				baby_out = baby_out >> 1;
+				baby_out = XNco_baby_Get_ekg_sample_V(&baby)<<8;
+
 
 				if(baby_address==baby_size){
 					baby_address=0;
@@ -332,13 +333,14 @@ void both_beat(void) //both heart beats
 					baby_address=baby_address+1;
 				}
 
+				temp = (mom_out+baby_out);
 	//		/* Add scaled noise component to the L+R audio samples */
 	//		out_left =  temp + in_left;
 	//		out_right = temp + in_right;
 
 			/* Output corrupted audio to the codec */
-			Xil_Out32(I2S_DATA_TX_L_REG, mom_out+baby_out);
-			Xil_Out32(I2S_DATA_TX_R_REG, mom_out+baby_out);
+			Xil_Out32(I2S_DATA_TX_L_REG, temp);
+			Xil_Out32(I2S_DATA_TX_R_REG, temp);
 
 
 
@@ -377,7 +379,7 @@ void filter(void) //just mom
 	static const u32 baby_size = 19410;
 	static u32 baby_address = 0;
 	static u32 mom_address = 0;
-	u32 baby_out, mom_out, in_left, prev_left, out_left, filter_out;
+	u32 baby_out, mom_out, in_left, prev_left, filter_out, temp;
 
 	while (!XUartPs_IsReceiveData(UART_BASEADDR)){
 	/* Sample L+R audio from the codec */
@@ -403,18 +405,19 @@ void filter(void) //just mom
 			/* Receive sinusoidal sample from NCO core */
 			mom_out = XNco_mom_Get_ekg_sample_V(&mom);
 
-			if(mom_address==mom_size){
-				mom_address=0;
-			}
-			else{
+
+				if(mom_address==mom_size){
+					mom_address=0;
+				}
+				else{
 				mom_address=mom_address+1;
-			}
+				}
 
 			XNco_baby_Set_address_V(&baby, baby_address);
 
 				/* Receive sinusoidal sample from NCO core */
 				baby_out = XNco_baby_Get_ekg_sample_V(&baby);
-				baby_out = baby_out >> 1;
+				temp = mom_out+baby_out;
 
 				if(baby_address==baby_size){
 					baby_address=0;
@@ -423,8 +426,8 @@ void filter(void) //just mom
 					baby_address=baby_address+1;
 				}
 			XLms_hw_Set_noise_in_V(&lms, mom_out);
-			XLms_hw_Set_signal_noise_V(&lms, mom_out+baby_out);
-			filter_out = XLms_hw_Get_output_V(&lms);
+			XLms_hw_Set_signal_noise_V(&lms, temp);
+			filter_out = XLms_hw_Get_output_V(&lms)<<8;
 
 
 
@@ -441,9 +444,9 @@ void filter(void) //just mom
 			//XNco_Get_sine_sample_V(&Nco);
 
 			//temp = nco_out;
-
+			prev_left = in_left;
 		}
-	prev_left = in_left;
+
 		/* If the DIP switch values have changed, break from while
 		 * loop to allow the step size value to update.
 		 */
@@ -452,39 +455,40 @@ void filter(void) //just mom
 	 * Else, continue. */
 	if(XUartPs_ReadReg(UART_BASEADDR, XUARTPS_FIFO_OFFSET) == 'q') menu();
 	else filter();
+}
 
 
-///* ---------------------------------------------------------------------------- *
-// * 								audio_stream()									*
-// * ---------------------------------------------------------------------------- *
-// * This function performs audio loopback streaming by sampling the input audio
-// * from the codec and then immediately passing the sample to the output of the
-// * codec.
-// *
-// * The main menu can be accessed by entering 'q' on the keyboard.
-// * ---------------------------------------------------------------------------- */
-//void audio_stream(){
-//
-//	//int index = 0;
-//	u32  in_left, in_right;
-//
-//	while (!XUartPs_IsReceiveData(UART_BASEADDR)){
-//
-//		// Read audio input from codec
-//		in_left = Xil_In32(I2S_DATA_RX_L_REG);
-//		in_right = Xil_In32(I2S_DATA_RX_R_REG);
-//
-//		// Write audio input to codec
-//		Xil_Out32(I2S_DATA_TX_L_REG, in_left);
-//		Xil_Out32(I2S_DATA_TX_R_REG, in_right);
-//
-//	}
-//
-//	/* If input from the terminal is 'q', then return to menu.
-//	 * Else, continue streaming. */
-//	if(XUartPs_ReadReg(UART_BASEADDR, XUARTPS_FIFO_OFFSET) == 'q') menu();
-//	else audio_stream();
-//} // audio_stream()
+/* ---------------------------------------------------------------------------- *
+ * 								audio_stream()									*
+ * ---------------------------------------------------------------------------- *
+ * This function performs audio loopback streaming by sampling the input audio
+ * from the codec and then immediately passing the sample to the output of the
+ * codec.
+ *
+ * The main menu can be accessed by entering 'q' on the keyboard.
+ * ---------------------------------------------------------------------------- */
+void audio_stream(){
+
+	//int index = 0;
+	u32  in_left, in_right;
+
+	while (!XUartPs_IsReceiveData(UART_BASEADDR)){
+
+		// Read audio input from codec
+		in_left = Xil_In32(I2S_DATA_RX_L_REG);
+		in_right = Xil_In32(I2S_DATA_RX_R_REG);
+
+		// Write audio input to codec
+		Xil_Out32(I2S_DATA_TX_L_REG, in_left);
+		Xil_Out32(I2S_DATA_TX_R_REG, in_right);
+
+	}
+
+	/* If input from the terminal is 'q', then return to menu.
+	 * Else, continue streaming. */
+	if(XUartPs_ReadReg(UART_BASEADDR, XUARTPS_FIFO_OFFSET) == 'q') menu();
+	else audio_stream();
+} // audio_stream()
 
 
 /* ---------------------------------------------------------------------------- *
